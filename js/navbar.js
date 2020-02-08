c11b4e07bb2021f1a3e37d909eb85baad989b786
Francisco <fcsuarez96@gmail.com>
2020-02-08 11:46:59 +0100

    Working on header

--- js/navbar.js
+++ js/navbar.js
@@ -1,49 +1,24 @@
 
-
-//W3SCHOOL Smooth scroll
-$(document).ready(function(){
-
-  $("a").on('click', function(event) {
-
-
-    if (this.hash !== "") {
-
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-
-        window.location.hash = hash;
-      });
-    }
-  });
-});
-
-
-  // Closes mobile menu when you click a link
+  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
 
-// Change navbar when you scroll down
-var navbarCollapse = function() {
-  if ($("#mainNav").offset().top > 100) {
-    $("#mainNav").addClass("navbar-shrink");
-  } else {
-    $("#mainNav").removeClass("navbar-shrink");
-  }
-};
-// Collapse now if page is not at top
-navbarCollapse();
-// Collapse the navbar when page is scrolled
-$(window).scroll(navbarCollapse);
+  // Add a class to the navbar so when we scroll down it changes
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+
 
 
 
