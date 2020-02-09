0e614c6d5166b65efdc5151a34808aaf6a5aefd7
Francisco <fcsuarez96@gmail.com>
2020-02-09 21:18:03 +0100

    History section

--- js/navbar.js
+++ js/navbar.js
@@ -3,10 +3,13 @@
   // Add a class to the navbar so when we scroll down it changes
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
+
       $("#mainNav").addClass("navbar-shrink");
     } else {
+
       $("#mainNav").removeClass("navbar-shrink");
     }
+
   };
 
   navbarCollapse();
